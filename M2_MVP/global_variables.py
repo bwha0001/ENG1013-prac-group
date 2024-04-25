53330d3d7463f1ff216b7b57d350c0277c3faa3e
bwha0001 <164017505+bwha0001@users.noreply.github.com>
2024-04-25 23:38:47 +1000

    Update global_variables.py

--- M2_MVP/global_variables.py
+++ M2_MVP/global_variables.py
@@ -1,16 +1,14 @@
-<<<<<<< Updated upstream
 #File for storage of global varibles
 #Authors: Team F12
 #Version: 1 - Implemented for the button
 #Last Edit: 25 April 2024
 
-=======
+
 """
     This module holds global variables that are required to make the call back function to run the 
     ped_button function work in order to have an interrupt so the pedestrian button can be 
     pressed at any time as long as the polling loop is active
 """
->>>>>>> Stashed changes
 
 # Define your global variables here
 pedsPresent = None
