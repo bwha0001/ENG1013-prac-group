501911e844a9446b3a7c7f9c3d2762bb1f6574d3
bwha0001 <164017505+bwha0001@users.noreply.github.com>
2024-04-29 18:26:07 +1000

    Update temperature_handling.py

--- M3/temperature_handling.py
+++ M3/temperature_handling.py
@@ -1,8 +1,15 @@
 # translates the temperature from voltage to temperature.
+
+# Double check formulas, rearanged values from
+# https://learning.monash.edu/course/view.php?id=7529&section=5
+# thermistor reference
 import math as mth
 def temperature(tempVoltage):
-    if tempVoltage>26:
-        temperature = -7.017*mth.log(tempVoltage) + 41.128
-    if tempVoltage<=26:
-        temperature = -21.21*mth.log(tempVoltage) + 72.203
+    if tempVoltage>4.59:
+        resistance = -21.21*mth.log((0.0331335)*(-100+100/tempVoltage))
+    if tempVoltage<=4.59:
+        temperature = -7.015*mth.log((0.00284784)*(-100+500/tempVoltage))
+
+    # Hayley, sorry I got side tracked with the voltage. If you do a voltage dividor with a 1k resistor
+    # before the thermistor and take voltage across thermistor the formul
     return temperature
