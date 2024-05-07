410ba79c725aab57a62aed8b6d6da6a2c772f1e3
HJDusting <166684929+HJDusting@users.noreply.github.com>
2024-05-08 09:17:01 +1000

    Update temperature_handling.py

--- M3/temperature_handling.py
+++ M3/temperature_handling.py
@@ -1,15 +1,20 @@
 # translates the temperature from voltage to temperature.
 
 # Double check formulas, rearanged values from
-# https://learning.monash.edu/course/view.php?id=7529&section=5
 # thermistor reference
 import math as mth
+
+#voltage dividor with a 1k resistor to find voltage 
+def thermistor_resistance(thermRes, voltIn):
+    fixedRes = 1000
+    tempVoltage = voltIn * (fixedRes/(thermRes+fixedRes)) 
+    return tempVoltage
+
 def temperature(tempVoltage):
     if tempVoltage>4.59:
-        resistance = -21.21*mth.log((0.0331335)*(-100+100/tempVoltage))
+        #need to check constants
+        tempCelcius = -21.21*mth.log((0.0331335)*(-100+100/tempVoltage))
     if tempVoltage<=4.59:
-        temperature = -7.015*mth.log((0.00284784)*(-100+500/tempVoltage))
+        tempCelcius = -7.015*mth.log((0.00284784)*(-100+500/tempVoltage))
 
-    # Hayley, sorry I got side tracked with the voltage. If you do a voltage dividor with a 1k resistor
-    # before the thermistor and take voltage across thermistor the formul
-    return temperature
+    return tempCelcius
