5c5d398b0055b4615e605d9e35819a932a5864ce
CMacdonald002 <cmac0056@student.monash.edu>
2024-05-10 20:00:56 +1000

    led_state changed over to shift register

--- M3/led_state.py
+++ M3/led_state.py
@@ -6,124 +6,121 @@
 import time
 from pymata4 import pymata4
 
-#function for flashing to turn LED on and off every 0.5 seconds (2Hz)
-def flashing_led(board,pin):
-    """
-    Used to make LED flash at 2Htz for 1 second
-    
-    Args:
-        board: Arduino Set Up
-        pin (interger): Arduino Pin which LED is connected to
-    """    
-    
-    board.digital_write(pin,1)
-    time.sleep(0.3)
-    board.digital_write(pin,0)
-    time.sleep(0.3)
-
-
-#function to convert traffic light state into LED state
 def light_setting_state(board, changeableConditions, mainState, sideState, pedestrianState):
-    #readded modes to function call based on the function header
     """
-    Used to set traffic light state to on/off/flashing for each LED on the Arduino
+    Used to set traffic light state to on/off/flashing(if applicable) for each LED on the Arduino
+    Note: sideRed and pedestrianRed opperate together
         Args:
             board: Arduino Set Up
             board2: 2nd Arduino Set Up
             changeableConditions (dictonary): Anything related to the system that changes
-            main traffic light state ('red','yellow','green','flashing','off'): state/colour of light set
-            side traffic light state ('red','yellow','green','flashing','off'): state/colour of light set
-            pedestrian light state ('red','green','flashing', ,'off'): state/colour of light set
+            main traffic light state ('red','yellow','green','off'): state/colour of light set
+            side traffic light state ('red','yellow','green','off'): state/colour of light set
+            pedestrian light state ('red','green','flashing','off'): state/colour of light set
         Returns:
-            None
     """
-    # importing the globals
-
-    #dictonary of pin assignment for ardino
-    ledPins = changeableConditions["arduinoPins"]
-
+    #test line
     print("test line entered led_setting_state")
-    '''
-    mainState = changableConditions['trafficStage']
-    if mainState == 1:
-        mainState = 'red' #currently should start as red
-    else:
-        mainState = 'green'
-    sideState = 'red'
-    pedestrianState = 'red'
-    pedCountRecord = changableConditions['pedCountRecord']
-    '''
 
-    ledPins = changeableConditions['arduinoPins']
+    
+    #Extract from the globals
+    #input order to shift r
+    ledPins = changeableConditions['circutConditions']["ledShiftOrder"]
+    
+    ser = changeableConditions["arduinoPins"]["ledSer"]
+    rclk = changeableConditions["arduinoPins"]["ledRclk"]
+    srclk = changeableConditions["arduinoPins"]["ledSrclk"]
+
+    #Create list for shift inputs
+    toLedShift =[0,0,0,0,0,0,0,0]
+
+    #Check for invalid input, if side state is red pedestrian state must also be red
+    if sideState == "red":
+        if pedestrianState == "red":
+            pass
+        elif pedestrianState != "red":
+            #print error message of invlalid combination and do not continue with light setting
+            print("!!!!!!!!!!\nError invlaid lights combination; side and pedestrian lights must both be set to red at the same time")
+            return
+
+
+###### TODO Decide how flashing should be handled for maintence mode ie through this function or not?
 
 #main traffic light
     if mainState == "red":
-        board.digital_write(ledPins["mainRed"], 1)
-        board.digital_write(ledPins["mainYellow"], 0)
-        board.digital_write(ledPins["mainGreen"], 0)
+        toLedShift[ledPins["mainRed"]] = 1
+        toLedShift[ledPins["mainYellow"]] = 0
+        toLedShift[ledPins["mainGreen"]] = 0
         # print("main red on")
         # print("main yellow off\nmain green off")
     elif mainState == "yellow":
-        board.digital_write(ledPins["mainRed"], 0)
-        board.digital_write(ledPins["mainYellow"], 1)
-        board.digital_write(ledPins["mainGreen"], 0)
+        toLedShift[ledPins["mainRed"]] = 0
+        toLedShift[ledPins["mainYellow"]] = 1
+        toLedShift[ledPins["mainGreen"]] = 0
         # print("main red off \nmain yellow on\nmain green off")
     elif mainState == "green":
-        board.digital_write(ledPins["mainRed"], 0)
-        board.digital_write(ledPins["mainYellow"], 0)
-        board.digital_write(ledPins["mainGreen"], 1)
+        toLedShift[ledPins["mainRed"]] = 0
+        toLedShift[ledPins["mainYellow"]] = 0
+        toLedShift[ledPins["mainGreen"]] = 1
         # print("main red off \nmain yellow off\n main green on")
-    elif mainState == "flashing":
-        board.digital_write(ledPins["mainRed"], 0)
-        #board.pwm_write(ledPins["mainYellow"], 30)
-        flashing_led(board,ledPins["mainYellow"])
-        board.digital_write(ledPins["mainGreen"], 0)
-        # print("main red off \nmain yellow flashing\n main green off")
+        pass
     elif mainState == "off":
-        board.digital_write(ledPins["mainRed"], 0)
-        board.digital_write(ledPins["mainYellow"], 0)
-        board.digital_write(ledPins["mainGreen"], 0)
+        toLedShift[ledPins["mainRed"]] = 0
+        toLedShift[ledPins["mainYellow"]] = 0
+        toLedShift[ledPins["mainGreen"]] = 0
         # print("main red off \nmain yellow off\n main green off")
 # side state
     if sideState == "red":
-        board.digital_write(ledPins["sideRed"], 1)
-        board.digital_write(ledPins["sideYellow"], 0)
-        board.digital_write(ledPins["sideGreen"], 0)
+        toLedShift[ledPins["side/PedRed"]] = 1
+        toLedShift[ledPins["sideYellow"]] = 0
+        toLedShift[ledPins["sideGreen"]] = 0
         # print("side red on \n side yellow off\n side green off")
     elif sideState == "yellow":
-        board.digital_write(ledPins["sideRed"], 0)
-        board.digital_write(ledPins["sideYellow"], 1)
-        board.digital_write(ledPins["sideGreen"], 0)
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["sideYellow"]] = 1
+        toLedShift[ledPins["sideGreen"]] = 0
         # print("side red off \n side yellow on\n side green off")
     elif sideState == "green":
-        board.digital_write(ledPins["sideRed"], 0)
-        board.digital_write(ledPins["sideYellow"], 0)
-        board.digital_write(ledPins["sideGreen"], 1)
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["sideYellow"]] = 0
+        toLedShift[ledPins["sideGreen"]] = 1
         # print("side red off \n side yellow off\n side green on")
-    elif sideState == "flashing":
-        board.digital_write(ledPins["sideRed"], 0)
-        flashing_led(board,ledPins["sideYellow"])
-        board.digital_write(ledPins["sideGreen"], 0)
-        # print("side red off \n side yellow flashing\n side green off")
     elif sideState == "off":
-        board.digital_write(ledPins["sideRed"], 0)
-        board.digital_write(ledPins["sideYellow"], 0)
-        board.digital_write(ledPins["sideGreen"], 0)
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["sideYellow"]] = 0
+        toLedShift[ledPins["sideGreen"]] = 0
         # print("side red off \nside yellow off\n side green off")
 # pedestrian light
     if pedestrianState == "red":
-        board.digital_write(ledPins["pedestrianRed"], 1)
-        board.digital_write(ledPins["pedestrianGreen"], 0)
+        toLedShift[ledPins["side/PedRed"]] = 1
+        toLedShift[ledPins["pedestrianFlashing"]] = 0
+        toLedShift[ledPins["pedestrianGreen"]] = 0
         # print("pedestrian light red on \npedestrian green off")
     elif pedestrianState == "green":
-        board.digital_write(ledPins["pedestrianRed"], 0)
-        board.digital_write(ledPins["pedestrianGreen"], 1)
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["pedestrianFlashing"]] = 0
+        toLedShift[ledPins["pedestrianGreen"]] = 1
         # print("pedestrian light red of \npedestrian green on")
     elif pedestrianState == "flashing":
-        board.digital_write(ledPins["pedestrianRed"], 0)
-        flashing_led(board,ledPins["pedestrianGreen"])
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["pedestrianFlashing"]] = 1
+        toLedShift[ledPins["pedestrianGreen"]] = 0
         #print("pedestrian light green off  \npedestrian green flashing")
     elif pedestrianState == "off":
-        board.digital_write(ledPins["pedestrianRed"], 0)
-        board.digital_write(ledPins["pedestrianGreen"], 0)
+        toLedShift[ledPins["side/PedRed"]] = 0
+        toLedShift[ledPins["pedestrianFlashing"]] = 0
+        toLedShift[ledPins["pedestrianGreen"]] = 0
         # print("pedestrian red off \n pedestrian green off")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+    #Send to shift register
+    for i in range(0,len(toLedShift)):
+        #Send to the shift(input) register
+        board.digital_pin_write(srclk,0)
+        board.digital_pin_write(ser, toLedShift[i])
+        time.sleep(0.001)
+        board.digital_pin_write(srclk,1)
+
+    #send to the storage(output) register
+    board.digital_pin_write(rclk, 1)
+    time.sleep(0.001)
+    board.digital_pin_write(rclk, 0)
