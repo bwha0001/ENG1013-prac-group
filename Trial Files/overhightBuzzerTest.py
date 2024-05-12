a3cb7d1f32a9454dd9ac63e34ea7b89713128a7e
bwha0001 <164017505+bwha0001@users.noreply.github.com>
2024-05-12 21:35:07 +1000

    Update overhightBuzzerTest.py

--- Trial Files/overhightBuzzerTest.py
+++ Trial Files/overhightBuzzerTest.py
@@ -4,44 +4,45 @@ import time, threading
 
 
 board = pymata4.Pymata4()
-
-board.set_pin_mode_digital_output(7)
-
-# print("on")
-# timeNow = time.time()
-
-# board.digital_write(7,1)
-# board.digital_write(8,1)
-# currentTime = timeNow-time.time()
-# time.sleep(1)
-# board.digital_write(7,0)
-# board.digital_write(8,1)
-
-def turn_off(pin):
-    print("################\noff\n###########")
-    board.digital_write(pin, 0)
-
-# Function to trigger the output
-def trigger_output():
-    # Turn on the output
-    output_pin = 7
-    print("on")
-    board.digital_write(output_pin, 1)
-    # Schedule turning off the output after 10 seconds
-    threading.Timer(5, turn_off, args=[output_pin]).start()
-
-# Call the function to trigger the output
-
-trigger_output()
-i=0
-while i<10:
-    i=i+1
-    print(i)
-    time.sleep(1)
-
-# # board.digital_write(7,0)
-# print("#############################")
-# print("off\n\n\n\n\n\n")
-
-
+pin = 8
+board.set_pin_mode_digital_output(pin)
+print("on")
+board.digital_write(pin,1)
+time.sleep(2)
+print("off")
+board.digital_write(pin,0)
 board.shutdown()
+# # print("on")
+# # timeNow = time.time()
+
+# # board.digital_write(7,1)
+# # board.digital_write(8,1)
+# # currentTime = timeNow-time.time()
+# # time.sleep(1)
+# # board.digital_write(7,0)
+# # board.digital_write(8,1)
+
+# def turn_off(pin):
+#     print("################\noff\n###########")
+#     board.digital_write(pin, 0)
+
+# # Function to trigger the output
+# def trigger_output():
+#     # Turn on the output
+#     output_pin = 7
+#     print("on")
+#     board.digital_write(output_pin, 1)
+#     # Schedule turning off the output after 10 seconds
+#     threading.Timer(5, turn_off, args=[output_pin]).start()
+
+# # Call the function to trigger the output
+
+# trigger_output()
+
+
+# # # board.digital_write(7,0)
+# # print("#############################")
+# # print("off\n\n\n\n\n\n")
+
+
+# board.shutdown()
